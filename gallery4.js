7b0a9a98013991cfe09b09cb9c4bd253a604bc6c
Shaji Kunjumohamed <shajikk@gmail.com>
2022-08-23 22:51:24 -0700

    New Portfolio changes

---
+++ gallery4.js
@@ -0,0 +1,29 @@
+import PhotoSwipeLightbox from 'https://cdn.jsdelivr.net/npm/photoswipe@5.3.0/dist/photoswipe-lightbox.esm.js';
+import PhotoSwipeDynamicCaption from './photoswipe-dynamic-caption-plugin.esm.js';
+
+const smallScreenPadding = {
+  top: 0, bottom: 0, left: 0, right: 0
+};
+const largeScreenPadding = {
+  top: 30, bottom: 30, left: 0, right: 0
+};
+const lightbox = new PhotoSwipeLightbox({
+  gallerySelector: '#gallery4',
+  childSelector: '.pswp-gallery4__item',
+  
+  // optionaly adjust viewport
+  paddingFn: (viewportSize) => {
+    return viewportSize.x < 700 ? smallScreenPadding : largeScreenPadding
+  },
+  pswpModule: () => import('https://cdn.jsdelivr.net/npm/photoswipe@5.3.0/dist/photoswipe.esm.js')
+});
+
+const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+  mobileLayoutBreakpoint: 700,
+  type: 'auto',
+  mobileCaptionOverlapRatio: 1
+});
+
+lightbox.init();
+
+
